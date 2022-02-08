1261e8fb279eb6c5b25f3a9318bfd73bfac8183b
webtracer <74070641+webtracer@users.noreply.github.com>
2022-02-08 09:04:16 -0800

    Add files via upload

---
+++ If_Statements/If_Statements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace If_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the temperature outside? ");
+            string temperature = Console.ReadLine();
+            int numTemp; // = int.Parse(temperature);
+            int num;
+
+            if (int.TryParse(temperature, out num))
+            {
+                numTemp = num;
+            }
+            else
+            {
+                numTemp = 0;
+                Console.WriteLine($"Value entered was not a number, it was {temperature}, temperature was set to 0");
+            }
+
+            if(numTemp < 40)
+            {
+                Console.WriteLine("You will need your coat today");
+            }else if(numTemp < 60)
+            {
+                Console.WriteLine("Pants and a hoodie should suffice");
+            }else if(numTemp > 90)
+            {
+                Console.WriteLine("Looks like a scorcher ... Enjoy!");
+            }else
+            {
+                Console.WriteLine("Shorts and tee shirt are good");
+            }
+
+            Console.Read();
+        }
+    }
+}
